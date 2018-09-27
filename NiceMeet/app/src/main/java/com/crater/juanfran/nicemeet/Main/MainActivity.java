4bea0ad99fd9a09d6c55f739f63c1234290d427c
JUAN FRANCISCO BENITEZ LOPEZ <6001139@insags.com>
2018-09-27 13:20:07 +0200

    Eliminando zip

---
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/Main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crater.juanfran.nicemeet.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.crater.juanfran.nicemeet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
