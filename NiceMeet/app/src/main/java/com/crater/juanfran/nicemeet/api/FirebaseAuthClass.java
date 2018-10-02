dda63d2e65cb140573dc0eeead7faea50feb4eac
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-10-02 11:54:12 +0200

    Estado compilable no funcional, activity de chat en estado beta, necesario pruebas (Sprint 3)

--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/api/FirebaseAuthClass.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/api/FirebaseAuthClass.java
@@ -31,7 +31,6 @@ public class FirebaseAuthClass {
                             listener.onError();
                         }
 
-                        // ...
                     }
                 });
     }
