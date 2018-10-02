227eb274e7b77f2af0ddb47431dec3c869b08c59
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-10-02 10:28:55 +0200

    ErrorDialog y estilo de este creado

--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/login/view/LoginActivity.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/login/view/LoginActivity.java
@@ -3,10 +3,12 @@ package com.crater.juanfran.nicemeet.login.view;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -30,6 +32,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.crater.juanfran.nicemeet.Main.MainActivity;
+import com.crater.juanfran.nicemeet.R;
 import com.crater.juanfran.nicemeet.Register.RegisterActivity;
 import com.crater.juanfran.nicemeet.login.contract.LoginContract;
 import com.crater.juanfran.nicemeet.utils.ValidatorsClass;
@@ -53,17 +57,20 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.vi
             Bundle bundle = new Bundle();
             bundle.putString("email",email);
         }
-
+        startActivity(intent);
     }
 
     @Override
     public void goMain() {
-
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onError(String error) {
-
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this, R.style.CookiesDialogCustom);
+        dialogBuilder.setTitle(getResources().getString(R.string.errorHappen));
+        dialogBuilder.setMessage(error);
     }
 }
 
