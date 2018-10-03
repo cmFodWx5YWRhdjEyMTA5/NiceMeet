c6e365d018f45bee8d09cbab5d98aaa5c077eb0a
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-10-03 13:47:33 +0200

    Principio de Fragments

--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/Main/MainActivity.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/Main/MainActivity.java
@@ -6,38 +6,51 @@ import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 
+import com.crater.juanfran.nicemeet.Main.Fragments.profile.ProfileFragment;
+import com.crater.juanfran.nicemeet.Main.Fragments.swipe.SwipeFragment;
 import com.crater.juanfran.nicemeet.R;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
-
+    FrameLayout frameLayout;
     boolean notifications =false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        frameLayout=findViewById(R.id.framelayu);
          bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
          bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
              @Override
              public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                  switch (item.getItemId()) {
                      case R.id.action_people:
+                         ft.replace(R.id.framelayu, new SwipeFragment());
+                         ft.commit();
                          break;
                      case R.id.action_chats:
                          eliminateNotifications();
+                        // ft.replace(R.id.framelayu, new ());
+                        // ft.commit();
                          break;
                      case R.id.action_profile:
                          createFakeNotification();
+                         ft.replace(R.id.framelayu, new ProfileFragment());
+                         ft.commit();
                          break;
                      case R.id.action_settings:
+                         //ft.replace(R.id.framelayu, new SwipeFragment());
+                         //ft.commit();
                          break;
                  }
                  return true;
