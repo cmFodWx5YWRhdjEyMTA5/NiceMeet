bf351cd58ce610f766c4f3585d8f6e46a0fe0ff3
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-10-03 10:45:52 +0200

    Trabajando en Login, base terminada. Aun falta cambiar colores y posiciones, pero la idea esta.

--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/SplashActivity.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.crater.juanfran.nicemeet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crater.juanfran.nicemeet.Main.MainActivity;
+import com.crater.juanfran.nicemeet.login.view.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -13,23 +16,23 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-       // mAuth=FirebaseAuth.getInstance();
+       mAuth=FirebaseAuth.getInstance();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-     //   FirebaseUser user = mAuth.getCurrentUser();
-     //   updateUI(user);
+     FirebaseUser user = mAuth.getCurrentUser();
+     updateUI(user);
     }
 
     private void updateUI(FirebaseUser user) {
         if(user==null)
         {
-            //go to login/register
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         }else
         {
-            //go to main
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
         }
     }
 }
--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/login/view/LoginActivity.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/login/view/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.crater.juanfran.nicemeet.login.view;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
@@ -14,6 +15,12 @@ import com.crater.juanfran.nicemeet.utils.ValidatorsClass;
 
 public class LoginActivity extends AppCompatActivity implements LoginContract.view {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
 
     @Override
     public void goRegister(String email) {
