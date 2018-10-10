2c760a2b162c799786c2fa9a9b167af23d9ebd6c
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-10-10 14:42:19 +0200

    No valido pero lo dejo por si acaso en este commit

--- NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/ui/Splash/SplashActivity.java
+++ NiceMeet/app/src/main/java/com/crater/juanfran/nicemeet/ui/Splash/SplashActivity.java
@@ -3,9 +3,10 @@ package com.crater.juanfran.nicemeet.ui.Splash;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
-import com.crater.juanfran.nicemeet.ui.Main.view.MainActivity;
 import com.crater.juanfran.nicemeet.R;
+import com.crater.juanfran.nicemeet.ui.Main.view.MainActivity;
 import com.crater.juanfran.nicemeet.ui.login.view.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,11 +14,13 @@ import com.google.firebase.auth.FirebaseUser;
 public class SplashActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
        mAuth=FirebaseAuth.getInstance();
+       progressBar=findViewById(R.id.marker_progress);
     }
 
     @Override
@@ -25,16 +28,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onStart();
      FirebaseUser user = mAuth.getCurrentUser();
      updateUI(user);
-     finish();
     }
+
 
     private void updateUI(FirebaseUser user) {
         if(user==null)
         {
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+          startActivity(new Intent(SplashActivity.this, LoginActivity.class));
         }else
         {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+          startActivity(new Intent(SplashActivity.this, MainActivity.class));
         }
     }
 }
